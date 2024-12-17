fbd1550fdc409184278459e1d7e9ea9e32a6927f
Admin <Admin@DESKTOP-V88L61F>
2024-12-17 15:10:02 +0530

    error check

--- simple-springboot-app/src/main/java/com/springboot/config/StudentConfig.java
+++ simple-springboot-app/src/main/java/com/springboot/config/StudentConfig.java
@@ -1,5 +1,6 @@
 package com.springboot.config;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,6 +14,7 @@ public class StudentConfig implements WebMvcConfigurer {
 	@Autowired
 	private RequestHeaderInterceptor requestHeaderInterceptor;
 
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		// TODO Auto-generated method stub
